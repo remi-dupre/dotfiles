e2a6e09acaefdd7be76b0dff0dff822f9db7f867
Rémi Dupré <r.dupre@qwant.com>
2020-11-02 11:25:46 +0100

    scripts: update-wallpaper logs

--- scripts/update-wallpaper.py
+++ scripts/update-wallpaper.py
@@ -1,5 +1,5 @@
-#!/usr/bin/python3
-
+#!/usr/bin/env python3
+import sys
 import time
 from datetime import date
 from shutil import copyfile
@@ -8,13 +8,23 @@ import urllib.request
 import yaml
 from PIL import Image, ImageDraw, ImageFont
 from Xlib.display import Display
+from Xlib.error import DisplayConnectionError
 
 
-archive_dir = "/data/bing-wallpapers"
-file_path = "/usr/share/bing-wallpaper.png"
-meta_url = "https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=fr-FR"
+ARCHIVE_DIR = "/data/bing-wallpapers"
+TMP_FILE_PATH = "/tmp/wallpaper.png"
+FILE_PATH = "/usr/share/bing-wallpaper.png"
+META_URL = "https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=fr-FR"
 
-screen = Display(':0').screen()
+# Wait for main display to be attached to get screen geometry
+while True:
+    try:
+        screen = Display(":0").screen()
+        break
+    except DisplayConnectionError:
+        print("Waiting for main screen...", file=sys.stderr)
+        time.sleep(1)
+
 screen_width = screen.width_in_pixels
 screen_height = screen.height_in_pixels
 
@@ -25,22 +35,19 @@ while True:
         response = urllib.request.urlopen("http://perdu.com", timeout=1)
         break
     except urllib.request.URLError:
-        pass
-
-    time.sleep(0.1)
+        print("Waiting for internet...", file=sys.stderr)
+        time.sleep(1)
 
 # Get the image
-tmp_file_path = "/tmp/wallpaper.png"
-
-data = urllib.request.urlopen(meta_url)
+data = urllib.request.urlopen(META_URL)
 data = yaml.safe_load(data)
 url = "http://bing.com" + data["images"][0]["url"]
-urllib.request.urlretrieve(url, tmp_file_path)
+urllib.request.urlretrieve(url, TMP_FILE_PATH)
 
 # Archive the picture
 date_str = date.today().isoformat()
-archive_file = "{}/descriptions.txt".format(archive_dir)
-copyfile(tmp_file_path, "{}/{}.png".format(archive_dir, date_str))
+archive_file = "{}/descriptions.txt".format(ARCHIVE_DIR)
+copyfile(TMP_FILE_PATH, "{}/{}.png".format(ARCHIVE_DIR, date_str))
 
 with open(archive_file) as file:
     descriptions = yaml.safe_load(file)
@@ -57,16 +64,14 @@ with open(archive_file, "w") as file:
 
 # Add legend
 text = descriptions[date_str]
-img = Image.open(tmp_file_path)
+img = Image.open(TMP_FILE_PATH)
 img = img.convert("RGBA")
 img = img.resize((screen_width, screen_height), Image.ANTIALIAS)
 
 tmp = Image.new("RGBA", img.size, (0, 0, 0, 0))
 draw = ImageDraw.Draw(tmp)
 
-font = ImageFont.truetype(
-    font="/usr/share/fonts/noto/NotoSans-Regular.ttf", size=18
-)
+font = ImageFont.truetype(font="/usr/share/fonts/noto/NotoSans-Regular.ttf", size=18)
 text_size = draw.textsize(text, font=font)
 shape = (
     img.size[0] - text_size[0] - 6,
@@ -78,7 +83,9 @@ draw.rectangle(shape, fill=(0, 0, 0, 150))
 draw.text((shape[0] + 3, shape[1] + 3), text, font=font, fill=(255, 255, 255))
 
 img = Image.alpha_composite(img, tmp)
-img.save(tmp_file_path)
+img.save(TMP_FILE_PATH)
 
 # Update wallpaper
-copyfile(tmp_file_path, file_path)
+copyfile(TMP_FILE_PATH, FILE_PATH)
+
+print("Finished!")
