5a91bc68f17a62b54f601d2eed057d0e64b5680c
Rémi Dupré <remi@dupre.io>
2023-12-08 17:49:19 +0100

    fix pylsp + ruff compatibility

--- dotfiles/.config/nvim/lua/languages/python.lua
+++ dotfiles/.config/nvim/lua/languages/python.lua
@@ -5,10 +5,26 @@ vim.api.nvim_create_autocmd('FileType', {
     end
 })
 
-require('lspconfig').ruff_lsp.setup {
-    init_options = {
-        settings = {
-            args = {},
+
+local lspconfig = require('lspconfig')
+lspconfig.ruff_lsp.setup {}
+
+lspconfig.pylsp.setup {
+    settings = {
+        pylsp = {
+            plugins = {
+                autopep8 = { enabled = false },
+                pycodestyle = { enabled = false },
+                pyflakes = { enabled = false },
+                pylint = { enabled = false },
+                yapf = { enabled = false },
+                rope_autoimport = { enabled = true },
+                rope_completion = { enabled = true },
+            }
         }
-    }
+    },
+    on_attach = function(client, _)
+        -- Disable formatting in favor of ruff
+        client.server_capabilities.documentFormattingProvider = false
+    end,
 }
